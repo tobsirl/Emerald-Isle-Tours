aac5eb13842a3b5bfde7f06a5bded421cd373c92
Paul Tobin <tobster111@gmail.com>
2019-09-04 10:41:00 +0100

    Added app.use(express.json()); for parsing json data

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     success: 'success',
-    data: 'Welcome to the first endpoint'
+    message: 'Welcome to the first endpoint'
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
 });
 
 const port = 3000;
