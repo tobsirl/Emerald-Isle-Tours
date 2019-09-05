e3fe4ea11a54b521ad206cc33d50d5c8fe453847
Paul Tobin <tobster111@gmail.com>
2019-09-05 12:10:06 +0100

    Added routes for getAllUsers, createUser, getUser, updateUser and deleteUser

--- app.js
+++ app.js
@@ -24,6 +24,51 @@ app.post('/api/v1/tours', (req, res) => {
   res.send('Done');
 });
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+}
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+}
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+}
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+}
+
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
 const port = 3000;
 
 app.listen(port, () => {
