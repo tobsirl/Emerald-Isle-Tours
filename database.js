c6dbed4bef7bd6a3c253dbaa92f7c4779c05d042
Paul Tobin <tobster111@gmail.com>
2019-09-07 10:58:44 +0100

    Added database.js for handling the connection to the database

---
+++ database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { MONGODB, MONGODB_PASSWORD } = process.env;
+console.log(process.env.MONGODB);
+
+const DB = MONGODB.replace('<password>', MONGODB_PASSWORD);
+
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('Connected to the Database... 🤖');
+  } catch (err) {
+    console.log(err.message);
+    // Exit connection process
+    process.exit(1);
+  }
+};
+
+module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const connectDatabase = require('./database');
+
+// Make database connection
+connectDatabase();
 
 const port = process.env.PORT || 3000;
 
