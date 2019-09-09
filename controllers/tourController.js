80e1dc1849f3263c8be1049e85703fea102c162b
Paul Tobin <tobster111@gmail.com>
2019-09-09 10:41:03 +0100

    Filled out the createTour controller using async/await wrapped in a try/catch

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,5 @@
+const Tour = require('../models/tourModel');
+
 exports.getAllTours = (req, res) => {
   res.status(500).json({
     status: 'error',
@@ -5,11 +7,19 @@ exports.getAllTours = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined'
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
