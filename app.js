13eb10cb618e383a9035c5a1b52d421e790ec293
Paul Tobin <tobster111@gmail.com>
2019-09-10 10:12:07 +0100

    Added tour data to data along with pictures of each tour

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware');
+//   next();
+// });
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
