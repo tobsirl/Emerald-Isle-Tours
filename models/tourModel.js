63d9c31a025c2d29047e13586621944a0783b332
Paul Tobin <tobster111@gmail.com>
2019-09-10 10:27:30 +0100

    Built out the tourModel to include a lot more fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true
   },
-  rating: {
+  duration: {
+    type: String,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a max group size']
+  },
+  difficulty: {
+    type: Number,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
