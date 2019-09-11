77d56cc68dede42c2b8370e2d00193f08e9aa2ff
Paul Tobin <tobster111@gmail.com>
2019-09-11 11:33:01 +0100

    getAllTours using filtering of page, sort, limit and fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,8 +3,10 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
-    const tours = await Tour.find(req.query);
+    const tours = await Tour.find(queryObj);
 
     res.status(200).json({
       status: 'success',
