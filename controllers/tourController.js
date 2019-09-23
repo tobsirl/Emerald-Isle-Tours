6c6d2787aff4adab23dc224aa0deb548ef1b566e
Paul Tobin <tobster111@gmail.com>
2019-09-23 16:05:03 +0100

    APIfeatures class has filter, sort, limitfields and paginate

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,7 +15,7 @@ class APIFeatures {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    this.query.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
@@ -31,52 +31,39 @@ class APIFeatures {
 
     return this;
   }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
 }
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-    // Build the query
-    // Filtering
-
-    // console.log(JSON.parse(queryStr));
-
-    // let query = await Tour.find(JSON.parse(queryStr));
-
-    // // Sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   query = query.sort(sortBy);
-    //   // sort('price ratingsAverage)
-    // } else {
-    //   query = query.sort('-createdAt');
-    // }
-
-    // console.log('here 1');
-    // // Field limiting
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    // console.log('here 2');
-    // //Pagination
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-
-    // // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) throw new Error('This page does not exist');
-    // }
-
     // execute the query
-    const features = new APIFeatures(Tour.find(), req.query).filter().sort();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
     const tours = await features.query;
 
     // send response
