b670fd7b1a6155010a954492f18e5f85af21a32b
Paul Tobin <tobster111@gmail.com>
2019-09-24 16:01:49 +0100

    Added getMontlyPlan for providing information on the tours available in a particular year

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,12 +8,14 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
-  getTourStats
+  getTourStats,
+  getMonthlyPlan
 } = require('../controllers/tourController');
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
   .route('/')
