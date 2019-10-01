b0eb56985807c775b4f57c15ffca3d6b0b8765ba
Paul Tobin <tobster111@gmail.com>
2019-10-01 15:53:31 +0100

    Added the AppError class to utils, this class extends Error

--- app.js
+++ app.js
@@ -18,11 +18,16 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
-  });
-  next();
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+  // next();
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
 });
 
 app.use((err, req, res, next) => {
