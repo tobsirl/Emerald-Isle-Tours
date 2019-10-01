59930c828672c7e8149ce293dd532faf6a15d7a3
Paul Tobin <tobster111@gmail.com>
2019-10-01 15:16:10 +0100

    Middleware function to handle unhandled route using .all and * to caputure those routes

--- models/tourModel.js
+++ models/tourModel.js
@@ -10,8 +10,8 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters'],
-      validate: [validator.isAlpha, 'Tour name must only contain characters']
+      minlength: [10, 'A tour name must have more or equal then 10 characters']
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
