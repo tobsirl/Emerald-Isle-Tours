508a5b808b3f7cf103df262252be9401c092d920
Paul Tobin <tobster111@gmail.com>
2019-10-03 10:37:10 +0100

    Refactored to seperate functions sendErrorDev and sendErrorProd

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,19 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
@@ -5,16 +21,8 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack
-    });
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
+    sendErrorProd(err, res);
   }
 };
