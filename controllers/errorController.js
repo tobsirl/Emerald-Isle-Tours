646738bc8658c9a9a3df4cb368513769cdc50fb9
Paul Tobin <tobster111@gmail.com>
2019-10-03 10:48:03 +0100

    Checking if the error is operational or unknown and logging the error message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,24 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // Programming or other unknown error: don't leak error details to the client
+  } else {
+    // 1) Log error
+    console.err('Error', err);
+
+    // 2) Send generate message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
