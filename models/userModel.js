cb6a95aa48d1e01e4b4a8ff57cb690b93bacbf3b
Paul Tobin <tobster111@gmail.com>
2019-10-04 16:14:03 +0100

    Using validate to check if the passwords are the same, only works on save and create

--- models/userModel.js
+++ models/userModel.js
@@ -23,7 +23,14 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
   }
 });
 
