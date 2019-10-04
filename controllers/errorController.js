5fc8c46ecc53132119dff6823202d5beb2b71b7a
Paul Tobin <tobster111@gmail.com>
2019-10-04 11:10:43 +0100

    Handling the CastError which is thrown by Mongo when a bad id is sent, added handleCastErrorDB function

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -23,7 +30,7 @@ const sendErrorProd = (err, res) => {
     // 2) Send generate message
     res.status(500).json({
       status: 'error',
-      message: 'Something went wrong'
+      message: 'Something went wrong!'
     });
   }
 };
@@ -37,6 +44,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
     sendErrorProd(err, res);
   }
 };
