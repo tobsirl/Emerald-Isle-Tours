736d02eebdf3be0f73008f4b6bb87f4ccf679197
Paul Tobin <tobster111@gmail.com>
2019-10-05 11:44:06 +0100

    The correct way of creating a new user, this way the person signing up is unable to assign themselves a role

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,14 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // Wrong way - Any user signing up for set there own role
+  // const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
 
   res.status(200).json({
     status: 'success',
