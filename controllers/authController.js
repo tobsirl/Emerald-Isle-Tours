8a73de8315e50fbf2f8cb6630cddc76a2808b503
Paul Tobin <tobster111@gmail.com>
2019-10-07 11:21:08 +0100

    Authorization - Generating a resetToken with createPasswordResetToken and saving it to the database

--- controllers/authController.js
+++ controllers/authController.js
@@ -115,6 +115,8 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 
   // 2. Generate the random reset
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
   // 3. Send it to user's email
 });
 
