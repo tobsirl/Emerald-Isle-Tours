58450be33348b26e8a9bceebfe5df5a21589fc96
Paul Tobin <tobster111@gmail.com>
2019-10-08 10:54:00 +0100

    Reset Password - Create the hashedToken using crypto, find the user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto*');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -149,4 +150,21 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1. Get the user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  // 2. If token has not expired, and there is user, set the new password
+
+  // 3. Update changedPasswordAt property for the user
+
+  // 4. Log the user in, send JWT
+});
