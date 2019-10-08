fdf4b251408b697ede234e06e0e226032c59b4f7
Paul Tobin <tobster111@gmail.com>
2019-10-08 08:41:53 +0100

    Added email options from, to, subject and text

--- utils/email.js
+++ utils/email.js
@@ -1,17 +1,23 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = options => {
-  // 1. Create a transporter
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+  // 1. Create a transport
+  const transport = nodemailer.createTransport({
+    service: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USER,
       password: process.env.EMAIL_PASSWORD
     }
-    // Activate in gmail "less secure app" option
   });
 
   // 2. Define the email options
+  const mailOptions = {
+    from: 'Paul Tobin <paul@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
 
   // 3. Actually send the email
 };
