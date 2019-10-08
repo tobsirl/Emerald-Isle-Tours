1d9133e7e743fbed87d8f76352b0f2b8ae0b858a
Paul Tobin <tobster111@gmail.com>
2019-10-08 10:56:52 +0100

    Reset Password - If the user doesnt exist send an error message, update the password and passwordConfirm and reset the restToken and resetExpires. Save the user

--- controllers/authController.js
+++ controllers/authController.js
@@ -163,6 +163,14 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   });
 
   // 2. If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
 
   // 3. Update changedPasswordAt property for the user
 
