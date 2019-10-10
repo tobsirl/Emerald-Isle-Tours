1e6a10ed52237eebd92b35d21656452d6b2a53c8
Paul Tobin <tobster111@gmail.com>
2019-10-10 19:59:10 +0100

    Installed helemt, required it in app.js and used it as middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,6 +9,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+// Set security http headers
+app.use(helmet());
 
 // Middleware function
 if (process.env.NODE_ENV === 'development') {
