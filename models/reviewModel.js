b4b3ae4ed9e9bf133058b17565c7f71b1b6e1225
Paul Tobin <tobster111@gmail.com>
2019-10-16 12:11:49 +0100

    Adding parent referencing to the user model using mongoose.Schema.ObjectId, each review must have a user

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -18,6 +18,11 @@ const reviewSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'Tour',
     required: [true, 'Review must belong to a tour']
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user']
   }
 });
 
