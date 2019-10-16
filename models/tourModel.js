e49af335fb1cf411d7c80f8b04fb6312c358a269
Paul Tobin <tobster111@gmail.com>
2019-10-16 11:31:11 +0100

    Using child referencing, guides is an array of mongoose.Schema.ObjectId with reference to the User

--- models/tourModel.js
+++ models/tourModel.js
@@ -108,7 +108,7 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: Array
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
   },
   {
     toJSON: { virtuals: true },
@@ -126,11 +126,11 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
