3eabfb041e7e819193838fbbaf280e9b6537a6aa
Paul Tobin <tobster111@gmail.com>
2019-12-02 17:20:52 +0000

    Fixed up import-dev-data to populate the database with both Users and Reviews

--- data/import-dev-data.js
+++ data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
 
 dotenv.config({ path: '../config.env' });
 
@@ -28,17 +30,17 @@ const DB = MONGODB.replace('<password>', MONGODB_PASSWORD);
   const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
   );
-  // const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-  // const reviews = JSON.parse(
-  //   fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
-  // );
+  const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+  const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+  );
 
   // import data into the database
   const importData = async () => {
     try {
       await Tour.create(tours);
-      // await User.create(users, { validateBeforeSave: false });
-      // await Review.create(reviews);
+      await User.create(users, { validateBeforeSave: false });
+      await Review.create(reviews);
       console.log('Data successfully loaded');
     } catch (error) {
       console.log(error);
@@ -50,8 +52,8 @@ const DB = MONGODB.replace('<password>', MONGODB_PASSWORD);
   const deleteData = async () => {
     try {
       await Tour.deleteMany();
-      // await User.deleteMany();
-      // await Review.deleteMany();
+      await User.deleteMany();
+      await Review.deleteMany();
       console.log('Data successfully deleted');
     } catch (error) {
       console.log(error);
